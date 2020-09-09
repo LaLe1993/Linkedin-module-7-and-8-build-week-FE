0ffb6d55d8debb2380284346915ab1767debea8c
LaLe1993 <63840101+LaLe1993@users.noreply.github.com>
2020-09-09 11:52:06 +0200

    SignUp page update

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import {FormGroup,FormControl, Button, Container, Row} from "react-bootstrap"
+import {FormGroup,FormControl,FormLabel, Button, Container, Row, Col,Card, Image} from "react-bootstrap"
 
 const apiKey = process.env.BE_API_LOCAL;
 class SignUp extends Component {
@@ -42,61 +42,76 @@ class SignUp extends Component {
 
     render() {
         return(
-          <Container className='d-flex justify-content-center '>
+          <Container className='mt-3'>
             <Row>
-            <form onSubmit={this.addNewUser}>
-                  <FormGroup controlId='username' bsSize='large'>
-                    <label>Username</label>
-                    <FormControl
-                      autoFocus
-                      id='username'
-                      type='text'
-                      value={this.state.newUser.username}
-                      onChange={(e) => this.handleChange(e)}
-                    />
-                  </FormGroup>
-                  <FormGroup controlId='name' bsSize='large'>
-                    <label>Name</label>
-                    <FormControl
-                      autoFocus
-                      id='name'
-                      type='text'
-                      value={this.state.newUser.name}
-                      onChange={(e) => this.handleChange(e)}
-                    />
-                  </FormGroup>
-                  <FormGroup controlId='surname' bsSize='large'>
-                    <label>Surname</label>
-                    <FormControl
-                      autoFocus
-                      id='surname'
-                      type='text'
-                      value={this.state.newUser.surname}
-                      onChange={(e) => this.handleChange(e)}
-                    />
-                  </FormGroup>
-                  <FormGroup controlId='email' bsSize='large'>
-                    <label>Email</label>
-                    <FormControl
-                      value={this.state.newUser.email}
-                      onChange={(e) => this.handleChange(e)}
-                      type='text'
-                      id='email'
-                    />
-                  </FormGroup>
-                  <FormGroup controlId='password' bsSize='large'>
-                    <label>Password</label>
-                    <FormControl
-                      value={this.state.newUser.password}
-                      onChange={(e) => this.handleChange(e)}
-                      type='password'
-                      id='password'
-                    />
-                  </FormGroup>
-                  <Button block bsSize='large' type='submit'>
-                    Sign Up
-                  </Button>
-                </form>
+              <Col sm={12} md={10} lg={6} className='offset-sm-0 offset-md-1 offset-lg-3'>
+                <Card className='p-4 shadow' style={{borderRadius:'5%'}}>
+                  <div className='d-flex justify-content-center mb-3    '><Image src='https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo-2003%E2%80%932011.png' className='w-25'></Image></div>
+                  <form onSubmit={this.addNewUser}>
+                        <FormGroup controlId='username' bsSize='large' as={Row}>
+                          <FormLabel column sm="3">Username</FormLabel>
+                          <Col sm='9'>
+                            <FormControl
+                              autoFocus
+                              id='username'
+                              type='text'
+                              value={this.state.newUser.username}
+                              onChange={(e) => this.handleChange(e)}
+                            />
+                          </Col>
+                        </FormGroup>
+                        <FormGroup controlId='name' bsSize='large' as={Row}>
+                          <FormLabel column sm="3">Name</FormLabel>
+                          <Col sm='9'>
+                            <FormControl
+                              autoFocus
+                              id='name'
+                              type='text'
+                              value={this.state.newUser.name}
+                              onChange={(e) => this.handleChange(e)}
+                            />
+                          </Col>
+                        </FormGroup>
+                        <FormGroup controlId='surname' bsSize='large' as={Row}>
+                          <FormLabel column sm="3">Surname</FormLabel>
+                          <Col sm='9'>
+                            <FormControl
+                              autoFocus
+                              id='surname'
+                              type='text'
+                              value={this.state.newUser.surname}
+                              onChange={(e) => this.handleChange(e)}
+                            />
+                          </Col>
+                        </FormGroup>
+                        <FormGroup controlId='email' bsSize='large' as={Row}>
+                          <FormLabel column sm="3">Email</FormLabel>
+                          <Col sm='9'>
+                            <FormControl
+                              value={this.state.newUser.email}
+                              onChange={(e) => this.handleChange(e)}
+                              type='text'
+                              id='email'
+                            />
+                          </Col>
+                        </FormGroup>
+                        <FormGroup controlId='password' bsSize='large' as={Row}>
+                          <FormLabel column sm="3">Password</FormLabel>
+                          <Col sm='9'>
+                            <FormControl
+                              value={this.state.newUser.password}
+                              onChange={(e) => this.handleChange(e)}
+                              type='password'
+                              id='password'
+                            />
+                          </Col>
+                        </FormGroup>
+                        <Button block bsSize='large' type='submit'>
+                          Sign Up
+                        </Button>
+                      </form>
+                </Card>
+              </Col>
             </Row>
           </Container>
             
